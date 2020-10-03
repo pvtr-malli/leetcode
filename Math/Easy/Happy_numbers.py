1b163a31fbecdc2e44e1cf4534ba7121c15cf8bb
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2020-10-03 21:20:13 +0530

    Add files via upload

---
+++ Math/Easy/Happy_numbers.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 20:55:18 2020
+
+@author: ninjaac
+"""
+"""
+Write an algorithm to determine if a number n is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Return True if n is a happy number, and False if not.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+"""
+class Solution:
+    @staticmethod
+    def isHappy(n):
+        
+        if n==1:
+            return True
+        if n==0 :
+            return False
+        
+        logic=[1,4,16,37,20]
+        while n not in logic:
+            n=sum( int(c)**2 for c in str(n) )
+        if n==1: return True
+        else:return False
+        
+print(Solution().isHappy(17))  
+
+n=19
+sum=sum(int(ch)**2 for ch in str(n))     
+
+
