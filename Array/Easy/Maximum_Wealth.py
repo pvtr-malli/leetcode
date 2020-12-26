afdbc7ed26b6da26b248ce0be9c158fab60616ca
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2020-12-26 17:52:02 +0530

    Add files via upload

---
+++ Array/Easy/Maximum_Wealth.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 26 11:57:26 2020
+
+@author: ninjaac
+"""
+
+
+"""Maximum Wealth
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+ 
+
+Example 1:
+
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+"""
+class Solution:
+    @staticmethod
+    def maximumWealth(accounts):
+        max_wealth = 0
+        for i in accounts:
+            if sum(i) > max_wealth: max_wealth = sum(i)
+        print(max_wealth)
+Solution.maximumWealth([[1,2,3],[3,2,1],[7,3]])
